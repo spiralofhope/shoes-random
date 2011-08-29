7edd411081fe4b215dc71f0ed0a51061919f89aa
spiralofhope <spiralofhope@lavabit.com>
2011-08-28 19:13:26 -0700

    working checkin

--- rubylearning.org/pong/pong.rb
+++ rubylearning.org/pong/pong.rb
@@ -30,18 +30,47 @@ ball_radius = 7
 paddle_width = 60
 paddle_height = 4
 playing_field_boundery = 6
-
+ball_speed = 10
 
 #
-#y_ball_limit_down  = ( window_height - 10 - () )
-#x_ball_limit_right = ( window_width  - 10 - paddle_width )
-#y_ball_limit_up = playing_field_boundery + ( ball_radius / 2 )
-#x_ball_limit_left = y_ball_limit_up
-
+#
 y_paddle_limit_down  = ( window_height - paddle_height - playing_field_boundery - 5 )
 x_paddle_limit_right = ( window_width  - paddle_width  - playing_field_boundery - 3 )
 y_paddle_limit_up = y_paddle_limit_down
 x_paddle_limit_left = ( playing_field_boundery + 3 )
+#
+# I'm not entirely sure what I'm doing.  =)
+y_ball_limit_down  = ( y_paddle_limit_down  - ball_radius - paddle_height - 4 )
+x_ball_limit_right = ( x_paddle_limit_right - ball_radius + paddle_width  - 4 )
+y_ball_limit_up   = playing_field_boundery + ( ball_radius / 2 ) + ( paddle_height / 2 ) + 4
+x_ball_limit_left = playing_field_boundery + ( ball_radius / 2 ) - ( paddle_height / 2 ) + 4
+# The ball starts at the top-left.
+ball_x = x_ball_limit_left
+ball_y = y_ball_limit_up
+# The ball begins moving to the bottom-right-ish.
+ball_angle = 90 + 45 # math is hard.
+
+def collide(
+              original_angle,
+              collision_wall
+  )
+p original_angle
+p collision_wall
+     if collision_wall == 'east' then
+    return x_ball_limit_right, 'TODO'
+  elsif collision_wall == 'west' then
+    return x_ball_limit_left, 'TODO'
+  elsif collision_wall == 'south' then
+    return y_ball_limit_up, 'TODO'
+  elsif collision_wall == 'north' then
+    return y_ball_limit_down, 'TODO'
+  else
+    puts "collision error"
+    ball_x = 100
+    ball_y = 100
+  end
+end
+
 
 # 1. Open Shoes Window
 Shoes.app(
@@ -75,8 +104,8 @@ Shoes.app(
   #ball_x = ( width  / 2 ) - ( ball_radius / 2 )
   #ball_y = ( height / 2 ) - ( ball_radius / 2 )
   # A ball appears (left-top side) and moves smoothly to right-bottom side at 20 frames per second.
-  ball_x = playing_field_boundery + ball_radius
-  ball_y = playing_field_boundery + ball_radius
+  ball_x = x_ball_limit_left
+  ball_y = y_ball_limit_up
   #
   @computer_paddle = (
     rect(
@@ -155,13 +184,19 @@ Shoes.app(
   animate( 20 ) do
     @ball.move( ball_x, ball_y )
     # Direction and speed.
-    ball_x += 5
-    ball_y += 5
-    # TODO: Check for a collision.
-    if ball_y > ( window_width - 10 - paddle_width ) then
-      ball_y = ( window_width - 10 - paddle_width )
+    ball_x += ball_speed
+    ball_y += ball_speed
+    # TODO: Check for a collision and do stuff with it.
+    # Restrict ball x
+    if    ball_x > x_ball_limit_right then ball_x, ball_angle = collide( ball_angle, 'east' )
+    elsif ball_x < x_ball_limit_left  then ball_x, ball_angle = collide( ball_angle, 'west' )
     end
-    #if ball_x
+    #
+    # Restrict ball y
+    if    ball_y > y_ball_limit_down then ball_y, ball_angle = collide( ball_angle, 'south' )
+    elsif ball_y < y_ball_limit_up   then ball_y, ball_angle = collide( ball_angle, 'north' )
+    end
+    p "#{ball_x}x #{ball_y}y"
   end
 
 # 3. Lock-in the ball within the window
@@ -175,12 +210,12 @@ Shoes.app(
     end
     #
     player_x = mouse_x
-    # Restrict x
+    # Restrict paddle x
     if player_x > x_paddle_limit_right then player_x = x_paddle_limit_right end
     if player_x < x_paddle_limit_left  then player_x = x_paddle_limit_left  end
     #
     player_y = mouse_y
-    # Restrict y
+    # Restrict paddle y
     if player_y > y_paddle_limit_down then player_y = y_paddle_limit_down end
     if player_y < y_paddle_limit_up   then player_y = y_paddle_limit_up   end
   end
